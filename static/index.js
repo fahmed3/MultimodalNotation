394c346179752653027e5559447f0ec6d065a5dc
Fabiha Ahmed <fsafabiha@gmail.com>
2021-02-18 14:05:16 -0500

    added audio playback

--- static/index.js
+++ static/index.js
@@ -4,11 +4,12 @@ let musicxml = "";
 let textinput = document.getElementById("textinput");
 let btn = document.getElementsByTagName("button")[0];
 
-
 //Keyboard shortcut, press alt+r to render output
-Mousetrap.bindGlobal('alt+r', function(e) {
+Mousetrap.bindGlobal("alt+r", function (e) {
   btn.click();
-})
+});
+
+var audioPlayer = new OsmdAudioPlayer(); //OsmdAudioPlayer.AudioPlayer()
 
 //Open Sheet Music Display used to render sheet music from music xml
 var openSheetMusicDisplay = new opensheetmusicdisplay.OpenSheetMusicDisplay(
@@ -30,31 +31,58 @@ form.addEventListener("submit", (event) => {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-    }, 
+    },
   })
-  // The POST request should result in updated global variables mxml and braille
-  // in app.py that get posted to /data
-  // We get the data with a GET request
-  // /data now contains {user_input:mxml, braille:braille}
-  .then((res) => {
-    console.log("Request complete! response:", res);
-    fetch("/data")
-      .then(function (response) {
-        return response.json();
-      })
-      // Convert musicxml to sheet music and display on the page
-      // Updated the braille 
-      .then(function (response) {
-        musicxml = response["user_input"];
-        braille = response["braille"];
-        openSheetMusicDisplay.load(musicxml).then(function () {
-          openSheetMusicDisplay.render();
-        });
+    // The POST request should result in updated global variables mxml and braille
+    // in app.py that get posted to /data
+    // We get the data with a GET request
+    // /data now contains {user_input:mxml, braille:braille}
+    .then((res) => {
+      console.log("Request complete! response:", res);
+      fetch("/data")
+        .then(function (response) {
+          return response.json();
+        })
+        // Convert musicxml to sheet music and display on the page
+        // Updated the braille
+        .then(function (response) {
+          musicxml = response["user_input"];
+          braille = response["braille"];
+          openSheetMusicDisplay
+            .load(musicxml)
+            .then(function () {
+              openSheetMusicDisplay.render();
+            })
+            .then(function () {
+              registerButtonEvents(audioPlayer);
+              audioPlayer.loadScore(openSheetMusicDisplay);
+              audioPlayer.on("iteration", (notes) => {
+                console.log(notes);
+              });
+            });
 
-        braille_sect.innerHTML = braille;
-      })
-      .catch((e) => console.log(e));
-  });
+          braille_sect.innerHTML = braille;
+        })
+        .catch((e) => console.log(e));
+    });
 
   event.preventDefault();
 });
+
+function registerButtonEvents(audioPlayer) {
+  document.getElementById("btn-play").addEventListener("click", () => {
+    if (audioPlayer.state === "STOPPED" || audioPlayer.state === "PAUSED") {
+      audioPlayer.play();
+    }
+  });
+  document.getElementById("btn-pause").addEventListener("click", () => {
+    if (audioPlayer.state === "PLAYING") {
+      audioPlayer.pause();
+    }
+  });
+  document.getElementById("btn-stop").addEventListener("click", () => {
+    if (audioPlayer.state === "PLAYING" || audioPlayer.state === "PAUSED") {
+      audioPlayer.stop();
+    }
+  });
+}
