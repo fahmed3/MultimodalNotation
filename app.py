88ff027782e501fdf81f529279256ff8d0dc2377
Fabiha Ahmed <fsafabiha@gmail.com>
2021-03-25 14:13:21 -0400

    updated to abcjs version

--- app.py
+++ app.py
@@ -4,86 +4,44 @@ from music21.braille import translate
 
 app = Flask(__name__)
 
+braille = ""
 fragment = """
 L:1/16
 M:3/4
 K:none
-D,4 D,E,F,^G, z4 | E12 |] """
-
-mxml = ""
-braille = ""
-errors = ""
-
+D,4 D,E,F,^G, z4 | E12 |]
+"""
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('editor-synth.html')
 
-
-# data will be used to store user input and
-# its output to transfer to and from index.js
 @app.route("/data", methods=['GET', 'POST'])
 def getuserinput():
-    # send the converted input to /data when there is a GET request
     if request.method == 'GET':
-        data = {"user_input": mxml, "braille": braille, "errors": errors}
-        return jsonify(data)
-
-    # POST request
-    # Accept POST request, used to take the user input from index.js
+        return "get request"
     if request.method == 'POST':
         data = request.get_json()['userdata']  # change to less vague name
-
+        print("received data: ", data)
         # Convert user input to the output
         convert_to_music_xml(data)
-
         return 'Success', 200
 
 
-# Converts user input to music XML and braille notation and
-# updates global variables
-# TODO - Experiment with warnings (e.g. note cannot show up in braille)
 def convert_to_music_xml(userinput):
-    global errors
-    global mxml
     global braille  
     try:
         # print("convert_to_musicxml")
         abcTextSample = converter.parse(userinput)
         braille = translate.objectToBraille(abcTextSample)
-        GEX = musicxml.m21ToXml.GeneralObjectExporter(abcTextSample)
-        out = GEX.parse()  # out is bytes
-        outStr = out.decode('utf-8')  # now is string
-        mxml = outStr.strip()
-        errors = ""
-        
-    # Error Handling
-    # Index Error - receives empty string
-    # converter.ConverterException - not abcNotation
-    # abcFormat.ABCHandlerException
-    except IndexError:  # Occurs when the user inputs nothing
-
-        ###### there is a check in home.js to see if input field is blank that
-        ###### keeps this stuff from being called
-
-        ###### anyway setting them back to "" is a bad fix
-        ###### find a way to reset the json server theyre sent to every render
-        ###### or how to actually pass input directly to getuserinput(), no global variables
-
-        error = 'Converter cannot parse, you inputted: "{}"'.format(userinput)
-        print(error)
-        mxml = ""
-        braille = ""
-        errors = error
+        print("Braille output: ", braille)
 
     # Send some kind of feedback to the user
-    except converter.ConverterException:
+    except converter.ConverterException as e:
+        print(e)
         error = "invalid syntax. unable to convert"
         print(error)
-        mxml = ""
         braille = ""
-        errors = error
-
 
 if __name__ == '__main__':
     app.debug = True
--- static/index.js
+++ static/index.js
@@ -1,31 +1,11 @@
-let form = document.getElementById("formid");
-let braille_sect = document.getElementById("brailleSection");
-let error_sect = document.getElementById("errorSection");
-let musicxml = "";
-let textinput = document.getElementById("textinput");
-let btn = document.getElementsByTagName("button")[0];
+let form = document.getElementById("formID");
+// let braille_sect = document.getElementById("brailleSection");
+let textinput = document.getElementById("abc");
 
-//Keyboard shortcut, press alt+r to render output
-Mousetrap.bindGlobal("alt+r", function (e) {
-  btn.click();
-});
-
-var audioPlayer = new OsmdAudioPlayer(); //OsmdAudioPlayer.AudioPlayer()
-
-//Open Sheet Music Display used to render sheet music from music xml
-var openSheetMusicDisplay = new opensheetmusicdisplay.OpenSheetMusicDisplay(
-  "osmdContainer"
-);
-openSheetMusicDisplay.setOptions({
-  backend: "svg",
-  drawingParameters: "compacttight", // don't display title, composer etc., smaller margins
-});
-
-// On form submit, we post the user input to /data,
-// allowing the POST request to be processed in app.py,
-// and we use a then statement to wait for a successful request.
 form.addEventListener("submit", (event) => {
+  event.preventDefault();
   let data = { userdata: textinput.value };
+  console.log(data);
   fetch("/data", {
     method: "POST",
     body: JSON.stringify(data),
@@ -33,60 +13,63 @@ form.addEventListener("submit", (event) => {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-  })
-    // The POST request should result in updated global variables mxml and braille
-    // in app.py that get posted to /data
-    // We get the data with a GET request
-    // /data now contains {user_input:mxml, braille:braille}
-    .then((res) => {
-      console.log("Request complete! response:", res);
-      fetch("/data")
-        .then(function (response) {
-          return response.json();
-        })
-        // Convert musicxml to sheet music and display on the page
-        // Updated the braille
-        .then(function (response) {
-          musicxml = response["user_input"];
-          braille = response["braille"];
-          errors = response["errors"];
-          if (musicxml.length > 0) {
-            openSheetMusicDisplay
-              .load(musicxml)
-              .then(function () {
-                openSheetMusicDisplay.render();
-              })
-              .then(function () {
-                registerButtonEvents(audioPlayer);
-                audioPlayer.loadScore(openSheetMusicDisplay);
-                audioPlayer.on("iteration", (notes) => {
-                  console.log(notes);
-                });
-              });
-          }
-          braille_sect.innerHTML = braille;
-          error_sect.innerHTML = errors;
-        })
-        .catch((e) => console.log(e));
-    });
-
-  event.preventDefault();
+  }).catch((e) => console.log(e));
 });
 
-function registerButtonEvents(audioPlayer) {
-  document.getElementById("btn-play").addEventListener("click", () => {
-    if (audioPlayer.state === "STOPPED" || audioPlayer.state === "PAUSED") {
-      audioPlayer.play();
-    }
+var abcjsEditor;
+
+window.onload = function () {
+  abcjsEditor = new ABCJS.Editor("abc", {
+    canvas_id: "paper",
+    warnings_id: "warnings",
+    synth: {
+      el: "#audio",
+      options: {
+        displayLoop: true,
+        displayRestart: true,
+        displayPlay: true,
+        displayProgress: true,
+        displayWarp: true,
+      },
+    },
+    abcjsParams: {
+      add_classes: true,
+      //   clickListener: clickListener,
+    },
+    selectionChangeCallback: selectionChangeCallback,
   });
-  document.getElementById("btn-pause").addEventListener("click", () => {
-    if (audioPlayer.state === "PLAYING") {
-      audioPlayer.pause();
-    }
-  });
-  document.getElementById("btn-stop").addEventListener("click", () => {
-    if (audioPlayer.state === "PLAYING" || audioPlayer.state === "PAUSED") {
-      audioPlayer.stop();
-    }
-  });
+};
+
+var lastNote = [];
+
+function selectionChangeCallback(start, end) {
+  if (abcjsEditor) {
+    var el = abcjsEditor.tunes[0].getElementFromChar(start);
+
+    if (!el) return;
+
+    //abcjs source code
+    var lastClicked = el.midiPitches;
+    if (!lastClicked) return; // returns when play button hasnt been pressed yet
+    // synth not initialized yet?
+
+    // if (lastClicked != lastNote) {
+    //only plays if cursor moves to new note
+
+    console.log(lastClicked);
+    ABCJS.synth
+      .playEvent(
+        lastClicked,
+        el.midiGraceNotePitches,
+        abcjsEditor.millisecondsPerMeasure()
+      )
+      .then(function (response) {
+        console.log("note played");
+        lastNote = lastClicked;
+      })
+      .catch(function (error) {
+        console.log("error playing note", error);
+      });
+    // }
+  }
 }
